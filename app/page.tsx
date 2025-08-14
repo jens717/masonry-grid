76f99b46ca33e755f5473f4428e26ba87d5164db
jens717 <151845602+jens717@users.noreply.github.com>
2025-08-14 17:53:37 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client";
+
+import Link from 'next/link'
+
 
 const page = () => {
   return (
-    <div className='text-4xl'>page</div>
+    <div className="flex flex-col h-full bg-[url('/photography-bg.jpeg')] 
+    bg-top bg-cover overflow-auto">
+      <header className="fixed top-0 w-full z-10 flex
+      justify-between items-center h-[90px] px-10">
+        <span className="uppercase text-lg font-medium">
+          Photography Portfolio
+        </span>
+        <Link href="#" 
+        className="rounded-3xl bg-white text-stone-700 
+        px-3 py-2 hover:bg-opacity-90">
+          Get in touch
+        </Link>
+      </header>
+    </div>
   )
 }
 
